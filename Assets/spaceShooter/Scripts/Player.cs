a51636512565fd587709fb5b276bc60e3ab5ec58
Jesús Eduardo Ferniz Cavazos <jesus_ferniz@hotmail.com>
2020-06-10 17:53:00 -0500

    Move player y meteoritos

---
+++ Assets/spaceShooter/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    
+    //Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(x, y, 0f) * speed * Time.deltaTime;
+    }
+}
