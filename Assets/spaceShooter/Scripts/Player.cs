ea29892cc3447ca3c45d26acd676ada7f3f40b06
Jesús Eduardo Ferniz Cavazos <jesus_ferniz@hotmail.com>
2020-06-17 14:56:40 -0500

    Spawner

--- Assets/spaceShooter/Scripts/Player.cs
+++ Assets/spaceShooter/Scripts/Player.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 5f;
-    
-    //Update is called once per frame
-    void Update()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(x, y, 0f) * speed * Time.deltaTime;
+    private Rigidbody2D rb;
+
+    float x;
+    float y;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        //transform.position += new Vector3(x, y, 0f) * speed * Time.deltaTime;
+
+        //rb.velocity = new Vector2(x, y) * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + new Vector3(x, y, 0f) * speed * Time.fixedDeltaTime);
     }
 }
---
+++ Assets/spaceShooter/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", 1f, 3f);
+    }
+    void Spawn()
+    {
+        Vector3 position = new Vector3(0f, 0f, 0f);
+        position.y = Random.Range(4.7f, 5f);
+        position.x = Random.Range(-3.25f, 3.61f);
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
